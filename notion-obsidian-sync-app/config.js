b6ce2adc68fe60f267ebaac59a00547475ac06a4
Amro Massalha <amro@beamr.com>
2024-04-28 12:27:13 +0300

    adding notion-obsidian-sync

---
+++ notion-obsidian-sync-app/config.js
@@ -0,0 +1,16 @@
+const config = {
+  obsidianVault: "/Users/amrom/Documents/Obsidian/AmroMassalha/notes/",
+  noteTagIds: [
+    { folder: "book", databaseId: "76dbf9080e2d4881b2f073c56167082a" },
+    { folder: "doc", databaseId: "f8d3de22ecbe491986244fcdf8302f6e" },
+    { folder: "fact", databaseId: "9363eca28a6042248d1292740e9a0171" },
+    { folder: "cheat-sheet", databaseId: "9d91fe150e994744a90ff722566adcea" },
+    {
+      folder: "code-snip",
+      databaseId: "05aa8b96e60e458999563d68913aa94c",
+    },
+    { folder: "prompt", databaseId: "db973cfe227d44e2a852c1fce2ae9a9b" },
+  ],
+};
+
+module.exports = { config };
