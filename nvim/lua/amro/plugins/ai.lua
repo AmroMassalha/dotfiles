4cf021e53ff1d6f0b8d420f9e2d203ad33fc4703
Amro Massalha <amro@beamr.com>
2024-05-11 22:46:45 +0300

    ai to nvim

---
+++ nvim/lua/amro/plugins/ai.lua
@@ -0,0 +1,13 @@
+return {
+  "David-Kunz/gen.nvim",
+  config = function()
+    local gen = require("gen")
+    gen.setup({
+      model = "codellama",
+    })
+    gen.prompts["devops me!"] = {
+      prompt = "You are a senior devops engineer, acting as an assistant. You offer help with cloud technologies like: Terraform, AWS, kubernetes, python. You answer with code examples when possible. $input:\n$text",
+      replace = true,
+    }
+  end,
+}
