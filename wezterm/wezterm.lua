f37b67bae5e21bf65c2a7bc0e296f6b399725107
Amro Massalha <amro@beamr.com>
2024-10-12 05:23:48 +0300

    test

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,15 +8,42 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 config.colors = {
-	foreground = "#CBE0F0",
-	background = "#011423",
-	cursor_bg = "#47FF9C",
-	cursor_border = "#47FF9C",
-	cursor_fg = "#011423",
-	selection_bg = "#706b4e",
-	selection_fg = "#f3d9c4",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+	-- Set foreground and background from the [colors.primary] section
+	foreground = "#edecee",  -- colors.primary.foreground
+	background = "#15141b",  -- colors.primary.background
+
+	-- Cursor colors from the [colors.cursor] section
+	cursor_bg = "#a277ff",    -- colors.cursor.cursor
+	cursor_border = "#a277ff", -- colors.cursor.cursor
+	cursor_fg = "#15141b",     -- Set to background color for contrast
+
+	-- Selection colors from the [colors.selection] section
+	selection_bg = "#29263c",  -- colors.selection.background
+	selection_fg = "#edecee",  -- Set to foreground for legibility
+
+	-- ANSI colors from the [colors.normal] section
+	ansi = {
+		"#110f18", -- black
+		"#ff6767", -- red
+		"#61ffca", -- green
+		"#ffca85", -- yellow
+		"#a277ff", -- blue
+		"#a277ff", -- magenta
+		"#61ffca", -- cyan
+		"#edecee", -- white
+	},
+
+	-- Bright ANSI colors from the [colors.bright] section
+	brights = {
+		"#4d4d4d", -- bright black
+		"#ff6767", -- bright red
+		"#61ffca", -- bright green
+		"#ffca85", -- bright yellow
+		"#a277ff", -- bright blue
+		"#a277ff", -- bright magenta
+		"#61ffca", -- bright cyan
+		"#edecee", -- bright white
+	},
 }
 
 config.keys = {
